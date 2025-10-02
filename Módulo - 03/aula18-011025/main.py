cf963d5c4bdc653f950ec535d2a1b0bfc4c016c6
thiagopecli <thiagopecli.jjboy@gmail.com>
2025-10-02 00:55:44 -0300

    finalizado

--- Módulo - 03/aula18-011025/main.py
+++ Módulo - 03/aula18-011025/main.py
@@ -16,3 +16,25 @@ acontecer!"""
 from Escola import Escola
 from Estudante import Estudante
 
+minhas_escola = Escola()
+print("Criando Alunos")
+
+aluno1 = Estudante(nome="Carlos", idade=30, matricula=202545)
+aluno2 = Estudante(nome="Amanda", idade=19, matricula=252545)
+print(f"Aluno '{aluno1.get_nome()}' criado. ")
+print(f"Aluno '{aluno2.get_nome()}' criado. ")
+
+print("Adicionando Notas")
+aluno1.adicionar_notas("Geografia", 7.2)
+aluno1.adicionar_notas("Geografia", 5)
+aluno1.adicionar_notas("Inglês", 9.2)
+
+aluno2.adicionar_notas("Historia", 9.2)
+aluno2.adicionar_notas("Geografia", 7)
+aluno2.adicionar_notas("Artes", 9.2)
+
+print("Matriculando os Alunos")
+minhas_escola.adicionar_estudante(aluno1)
+minhas_escola.adicionar_estudante(aluno2)
+
+minhas_escola.mostrar_relatorio()
