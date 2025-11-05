ef914d09af3f9486ff6c9b407201deb902dd23af
thiagopecli <148633999+thiagopecli@users.noreply.github.com>
2025-11-05 11:36:33 -0300

    update aula 05112025

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    context = {
+        'nome_usuario': 'Thiago',
+        'tecnologias':['Python','Django','HTML','CSS']
+    }
+    return render(request, 'home.html', context)
 
 def sobre(request):
     return HttpResponse("<h1>Sobre:</h1>"
