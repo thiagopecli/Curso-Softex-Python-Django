ac2c68bb244b16e61b86f2135ce943debbb15c1a
thiagopecli <148633999+thiagopecli@users.noreply.github.com>
2025-11-17 11:09:34 -0300

    feat: update aula

--- modulo4/core/urls.py
+++ modulo4/core/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path(
-        '',
-        views.home,
-        name='home',),
-        path(
-        'sobre',
-        views.sobre,
-        name='sobre,'),
+    path('', views.home, name='home'),
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
+    path('sobre', views.sobre, name='sobre,'),
 ]
--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Tarefa
 from .models import Execucao
 from .forms import TarefaForm
 # Create your views here.
+
 def home(request):
     if request.method == 'POST':
         form = TarefaForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
             return redirect('home')
@@ -19,7 +20,7 @@ def home(request):
     todas_as_execucoes = Execucao.objects.all()
     context = {
         'nome_usuario': 'Thiago',
-        'tecnologias':['Python','Django','HTML','CSS'],
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
         'tarefas': todas_as_tarefas,
         'execucoes': todas_as_execucoes,
         'form': form,
@@ -28,4 +29,26 @@ def home(request):
 
 def sobre(request):
     return HttpResponse("<h1>Sobre:</h1>"
-    "<h2> Django é uma framework de alto nível.</h2>")
+                        "<h2> Django é uma framework de alto nível.</h2>")
+
+def concluir_tarefa(request, pk):
+        # 1. Busca a tarefa pela 'pk' (ID) vinda da URL.
+        # Se não achar, retorna um erro 404.
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    # 2. Segurança: Apenas execute se o método for POST
+    if request.method == 'POST':
+        # 3. A Lógica de "Update"
+        tarefa.concluida = True
+        tarefa.save()  # Não se esqueça de salvar!
+        # 4. Redireciona de volta para a 'home' (Padrão PRG)
+    return redirect('home')
+
+def deletar_tarefa(request, pk):
+    # 1. Busca a tarefa
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+        # 2. Segurança: Apenas execute se o método for POST
+    if request.method == 'POST':
+        # 3. A Lógica de "Delete"
+        tarefa.delete()
+        # 4. Redireciona de volta para a 'home'
+    return redirect('home')
